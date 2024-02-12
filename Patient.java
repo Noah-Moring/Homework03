f0235247d2e3753b0f40fc8beb4ec3e9e9d64618
Noah-Moring <148168395+Noah-Moring@users.noreply.github.com>
2024-02-12 13:40:20 -0500

    Patient(1)

---
+++ Patient.java
@@ -0,0 +1,10 @@
+public class Patient{
+    public int idNumber;
+    public double caffeineLvl;
+
+    public Patient(int i, double c){
+        this.idNumber = i;
+        this.caffeineLvl = c;
+
+    }
+}
